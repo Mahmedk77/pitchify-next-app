33b202cca1c6037b1da919d5af406f1326953484
Mahmedk77 <mak.212601@gmail.com>
2025-10-05 18:42:49 +0500

    updated editor picks

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,12 +1,13 @@
 import Heading from '@/components/Heading';
 import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
-import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { STARTUP_BY_ID_QUERY, PLAYLIST_BY_SLUG_QUERY } from '@/sanity/lib/queries';
 import { notFound } from 'next/navigation';
 import markdownit from 'markdown-it'
 import React, { Suspense } from 'react'
 import { Skeleton } from '@/components/ui/skeleton';
 import View from '@/components/View';
+import StartupCard, { StartupCardType } from '@/components/StartupCard';
 
 export const experimental_ppr = true;
 
@@ -16,8 +17,12 @@ const page = async ({ params }: { params: Promise<{ id: string }>}) => {
 
     const id = (await params).id;
     // console.log(id);
-    const post =  await client.fetch(STARTUP_BY_ID_QUERY, { id });
     
+    const [post, { select: editorPosts }] = await Promise.all([
+        client.fetch(STARTUP_BY_ID_QUERY, { id }),
+        client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: 'new-editor-picks' })
+    ])
+        
 
     // console.log("post", post)
     if (!post) return notFound();
@@ -29,8 +34,8 @@ const page = async ({ params }: { params: Promise<{ id: string }>}) => {
       {/* ------------------HEADER-------------------------- */}
         <section className='header pattern'>
             <p className='tag'>{formatDate(post._createdAt)}</p>
-            <Heading text1={post.title} text2={""}/>
-            <p className='text-white my-6 px-8 text-center !text-xl md:px-42'>{post.description}</p>
+            <Heading text1={post.title} />
+            <p className='text-white my-6 px-0 text-center !text-xl md:px-42'>{post.description}</p>
         </section>
         {/* ----------------------BODY---------------------- */}
         <section className='px-6 lg:px-36 py-6'>
@@ -57,7 +62,7 @@ const page = async ({ params }: { params: Promise<{ id: string }>}) => {
                     {
                         parsedContent 
                         ? (
-                            <article className='mt-8 prose leading-loose tracking-normal text-justify  text-gray-800 text-md'
+                            <article className='mt-8 prose leading-loose tracking-normal text-justify  text-gray-800 text-lg'
                             dangerouslySetInnerHTML={{__html: parsedContent}} />) 
                         : (
                             <p>No details</p>
@@ -65,7 +70,22 @@ const page = async ({ params }: { params: Promise<{ id: string }>}) => {
                     }
                 </div>
             <hr className='my-10 mx-auto border-zinc-400 max-w-4xl'/>
-                    {/* -------------------TODO: EDITOR PICKS-------------------- */}
+                    {
+                        editorPosts.length > 0 && (
+                            <div>
+                                <p className='font-semibold text-3xl'>
+                                Editor Picks
+                                </p>
+                            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 mt-8'>
+                                {
+                                    editorPosts.map((post: StartupCardType, index: number) => (
+                                        <StartupCard key={index} post={post}/>
+                                    ))
+                                }
+                            </div>
+                            </div>
+                        )
+                    }
                 
             
             </div>
